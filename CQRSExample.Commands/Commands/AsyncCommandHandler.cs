579506ee51e4ccb125535c30c4ddfe16e26c021c
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-12-13 17:57:56 +0100

    Create general abstract async command handler for only handle asynchronous calls.

---
+++ CQRSExample.Commands/Commands/AsyncCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CQRSExample.Domain.Interfaces;
+
+namespace CQRSExample.Commands.Commands
+{
+    public abstract class AsyncCommandHandler<T> : CommandHandler<T> where T : ICommand
+    {
+        protected AsyncCommandHandler(IEventBus eventBus) : base(eventBus)
+        {
+        }
+
+        public sealed override void Handle(T command)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
