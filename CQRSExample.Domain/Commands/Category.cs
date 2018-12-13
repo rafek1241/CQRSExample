17ec265c0d19062c6b603877ede1fe914c649cad
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-12-13 17:18:55 +0100

    create categories commands.

---
+++ CQRSExample.Domain/Commands/Category.cs
@@ -0,0 +1,40 @@
+﻿using CQRSExample.Domain.Interfaces;
+using CQRSExample.Domain.Models;
+using System;
+
+namespace CQRSExample.Domain.Commands
+{
+    public class CreateCategory : ICommand
+    {
+        private readonly Category _category;
+
+        public CreateCategory(Category category)
+        {
+            category.Guid = Guid.NewGuid();
+
+            _category = category;
+        }
+    }
+
+    public class UpdateCategory : ICommand
+    {
+        private readonly long _id;
+        private readonly Category _category;
+
+        public UpdateCategory(long id, Category category)
+        {
+            _id = id;
+            _category = category;
+        }
+    }
+
+    public class RemoveCategory : ICommand
+    {
+        private readonly Category _category;
+
+        public RemoveCategory(Category category)
+        {
+            _category = category;
+        }
+    }
+}
