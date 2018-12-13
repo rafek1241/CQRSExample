98e82e6495be0f9671bf5a0e509ac97d4bc7d447
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-12-13 18:26:12 +0100

    Create general async event handler class to implement only async methods and seal the non-asynchronous method.

---
+++ CQRSExample.Events/Events/AsyncEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CQRSExample.Domain.Interfaces;
+
+namespace CQRSExample.Events.Events
+{
+    public abstract class AsyncEventHandler<T> : EventHandler<T> where T : IEvent
+    {
+        protected AsyncEventHandler(IEventBus eventBus) : base(eventBus)
+        {
+        }
+
+        public sealed override void Handle(T @event)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
