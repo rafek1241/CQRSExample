7486845900ab665e7c989f477fcd8084fefe2ef8
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-12-13 18:22:54 +0100

    Declare expected category events.

---
+++ CQRSExample.Domain/Events/CategoryEvents.cs
@@ -0,0 +1,45 @@
+﻿using CQRSExample.Domain.Interfaces;
+using CQRSExample.Domain.Models;
+
+namespace CQRSExample.Domain.Events
+{
+    public class ValidateCategory : IEvent
+    {
+        public Category Category { get; }
+
+        public ValidateCategory(Category category)
+        {
+            Category = category;
+        }
+    }
+
+    public class InsertCategory : IEvent
+    {
+        public Category Category { get; }
+
+        public InsertCategory(Category category)
+        {
+            Category = category;
+        }
+    }
+
+    public class UpdateCategory : IEvent
+    {
+        public Category Category { get; }
+
+        public UpdateCategory(Category category)
+        {
+            Category = category;
+        }
+    }
+
+    public class RemoveCategory : IEvent
+    {
+        public long Id { get; }
+
+        public RemoveCategory(long id)
+        {
+            Id = id;
+        }
+    }
+}
