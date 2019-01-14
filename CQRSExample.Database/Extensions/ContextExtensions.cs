dd54bf52e78c4f48731b6090cd130fb38b26ad39
Robert Szachnowski <rszachnowski@wi.ps.pl>
2019-01-14 17:03:19 +0100

    Create extension for DbContext.

---
+++ CQRSExample.Database/Extensions/ContextExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQRSExample.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CQRSExample.Database.Extensions
+{
+    public static class ContextExtensions
+    {
+        public static async Task<EntityEntry<T>> RemoveByIdAsync<T>(this DbSet<T> entitySet, long id) where T : Entity
+        {
+            var entity = await entitySet.SingleAsync(w => w.Id == id);
+            return entitySet.Remove(entity);
+        }
+
+        public static EntityEntry<T> RemoveById<T>(this DbSet<T> entitySet, long id) where T : Entity
+        {
+            var entity = entitySet.Single(w => w.Id == id);
+            return entitySet.Remove(entity);
+        }
+    }
+}
